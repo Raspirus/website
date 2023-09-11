0517b04fd6412d0ef5a57594dc77b729d10e1efd
Benjamin Demetz <50681275+Benji377@users.noreply.github.com>
2023-09-11 21:58:05 +0200

    Added RPi formats

--- components/Download.tsx
+++ components/Download.tsx
@@ -8,11 +8,34 @@ type Downloadprops = {
   size2: number | null;
 };
 
-export default function Download(
-  { title, image, desc, action, size, action2, size2 }: Downloadprops,
-) {
+export default function Download({
+  title,
+  image,
+  desc,
+  action,
+  size,
+  action2,
+  size2,
+}: Downloadprops) {
   const getFileExtension = (url: string) =>
     url.substring(url.lastIndexOf("/") + 1).split(".").pop();
+
+  const displayExtension = (url: string) => {
+    const extension = getFileExtension(url)?.toLowerCase();
+    const isDeb = extension === "deb";
+    const isArm64 = url.includes("arm64");
+    const isAmd64 = url.includes("amd64");
+
+    if (isDeb && !isAmd64) {
+      if (isArm64) {
+        return "ARM64";
+      } else {
+        return "ARMHF";
+      }
+    } else {
+      return extension?.toUpperCase();
+    }
+  };
 
   return (
     <div class="p-8 w-full md:w-1/2 lg:w-1/4 h-1/4 md:h-1/2 lg:h-full flex flex-col justify-center items-center">
@@ -32,23 +55,21 @@ export default function Download(
           itemProp="fileSize"
           class="bg-mainred text-white px-4 py-2 mt-10"
         >
-          {getFileExtension(action)?.toUpperCase()} -{" "}
-          {(size / 1048576).toFixed(2)}MB
+          {displayExtension(action)} - {(size / 1048576).toFixed(2)}MB
         </button>
       </a>
 
-      {action2 &&
-        (
-          <a itemProp="downloadUrl" href={action2}>
-            <button
-              itemProp="fileSize"
-              class="bg-maingreen text-white px-4 py-2 mt-2"
-            >
-              {getFileExtension(action2)?.toUpperCase()} -{" "}
-              {size2 ? (size2 / 1048576).toFixed(2) : "Error "}MB
-            </button>
-          </a>
-        )}
+      {action2 && (
+        <a itemProp="downloadUrl" href={action2}>
+          <button
+            itemProp="fileSize"
+            class="bg-maingreen text-white px-4 py-2 mt-2"
+          >
+            {displayExtension(action2)} -{" "}
+            {size2 ? (size2 / 1048576).toFixed(2) : "Error "}MB
+          </button>
+        </a>
+      )}
     </div>
   );
 }
