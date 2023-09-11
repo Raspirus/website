3082691caff051ad5c0e1f87bd7bedc694fbfed5
Benjamin Demetz <50681275+Benji377@users.noreply.github.com>
2023-09-11 21:51:53 +0200

    Added Raspberry Pi download options

--- routes/downloads.tsx
+++ routes/downloads.tsx
@@ -3,6 +3,12 @@ import Header from "../components/Header.tsx";
 import Navbar from "../components/Navbar.tsx";
 import { Handlers, PageProps } from "$fresh/server.ts";
 
+interface Asset {
+  name: string;
+  size: number;
+  browser_download_url: string;
+}
+
 interface GithubData {
   date: Date;
   version: string;
@@ -14,14 +20,16 @@ interface GithubData {
   mac_size: number;
   linux_link: string;
   linux_size: number;
-  linux_deb_link: string;
-  linux_deb_size: number;
-  raspberry_link: string;
-  raspberry_size: number;
+  linux_deb_arm64_link: string;
+  linux_deb_arm64_size: number;
+  linux_deb_armhf_link: string;
+  linux_deb_armhf_size: number;
+  linux_deb_amd_link: string;
+  linux_deb_amd_size: number;
 }
-// TODO: Fix Github Rate limit with personal auth token
+
 export const handler: Handlers<GithubData | null> = {
-  async GET(_, ctx) {
+  async GET(_: any, ctx: { render: (arg0: GithubData | null) => any; }) {
     const resp = await fetch(
       "https://api.github.com/repos/Raspirus/Raspirus/releases",
     );
@@ -35,21 +43,31 @@ export const handler: Handlers<GithubData | null> = {
     }
     const json_res = await resp.json();
     try {
+      // Extract data for different architectures
       const gdata: GithubData = {
         date: new Date(json_res[0].published_at),
         version: json_res[0].tag_name,
-        win_link: json_res[0].assets[3].browser_download_url,
-        win_size: json_res[0].assets[3].size,
-        win_msi_link: json_res[0].assets[5].browser_download_url,
-        win_msi_size: json_res[0].assets[5].size,
-        mac_link: json_res[0].assets[4].browser_download_url,
-        mac_size: json_res[0].assets[4].size,
-        linux_link: json_res[0].assets[0].browser_download_url,
-        linux_size: json_res[0].assets[0].size,
-        linux_deb_link: json_res[0].assets[1].browser_download_url,
-        linux_deb_size: json_res[0].assets[1].size,
-        raspberry_link: json_res[0].assets[2].browser_download_url,
-        raspberry_size: json_res[0].assets[2].size,
+        // EXE
+        win_link: json_res[0].assets.find(asset => asset.name.endsWith(".exe"))?.browser_download_url ?? "",
+        win_size: json_res[0].assets.find(asset => asset.name.endsWith(".exe"))?.size ?? 0,
+        // MSI
+        win_msi_link: json_res[0].assets.find(asset => asset.name.endsWith(".msi"))?.browser_download_url ?? "",
+        win_msi_size: json_res[0].assets.find(asset => asset.name.endsWith(".msi"))?.size ?? 0,
+        // DMG
+        mac_link: json_res[0].assets.find(asset => asset.name.endsWith(".dmg"))?.browser_download_url ?? "",
+        mac_size: json_res[0].assets.find(asset => asset.name.endsWith(".dmg"))?.size ?? 0,
+        // APPIMAGE
+        linux_link: json_res[0].assets.find(asset => asset.name.endsWith(".AppImage"))?.browser_download_url ?? "",
+        linux_size: json_res[0].assets.find(asset => asset.name.endsWith(".AppImage"))?.size ?? 0,
+        // ARM 64
+        linux_deb_arm64_link: json_res[0].assets.find(asset => asset.name.includes("_arm64.deb"))?.browser_download_url ?? "",
+        linux_deb_arm64_size: json_res[0].assets.find(asset => asset.name.includes("_arm64.deb"))?.size ?? 0,
+        // ARM HF
+        linux_deb_armhf_link: json_res[0].assets.find(asset => asset.name.includes("_armhf.deb"))?.browser_download_url ?? "",
+        linux_deb_armhf_size: json_res[0].assets.find(asset => asset.name.includes("_armhf.deb"))?.size ?? 0,
+        // AMD 64
+        linux_deb_amd_link: json_res[0].assets.find(asset => asset.name.endsWith("_amd64.deb"))?.browser_download_url ?? "",
+        linux_deb_amd_size: json_res[0].assets.find(asset => asset.name.endsWith("_amd64.deb"))?.size ?? 0,
       };
       return ctx.render(gdata);
     } catch (err) {
@@ -68,6 +86,7 @@ export default function Downloads({ data }: PageProps<GithubData | null>) {
   }-${year}`;
 
   const desc = `Version: ${data?.version}\nDate: ${formattedDate}`;
+  
   return (
     <>
       <Header title="Downloads | Raspirus" />
@@ -97,23 +116,24 @@ export default function Downloads({ data }: PageProps<GithubData | null>) {
             size2={null}
           />
           <Download
-            title="Debian/Linux"
+            title="Debian/Linux (AMD 64)"
             image="img/linux.png"
             desc={desc}
-            action={data?.linux_link ?? ""}
-            size={data?.linux_size ?? 0}
-            action2={data?.linux_deb_link ?? ""}
-            size2={data?.linux_deb_size ?? 0}
+            action={data?.linux_deb_amd_link ?? ""}
+            size={data?.linux_deb_amd_size ?? 0}
+            action2={data?.linux_link ?? ""}
+            size2={data?.linux_size ?? 0}
           />
           <Download
-            title="Raspberry Pi (ARM 64)"
+            title="Raspberry Pi"
             image="img/raspberrypi.png"
             desc={desc}
-            action={data?.raspberry_link ?? ""}
-            size={data?.raspberry_size ?? 0}
-            action2={null}
-            size2={null}
+            action={data?.linux_deb_arm64_link ?? ""}
+            size={data?.linux_deb_arm64_size ?? 0}
+            action2={data?.linux_deb_armhf_link ?? ""}
+            size2={data?.linux_deb_armhf_size ?? 0}
           />
+          {/* Add more Download components for other architectures if needed */}
         </div>
       </div>
     </>
